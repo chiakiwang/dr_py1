4d5f3588d1e9518ace31550dae171cb10f1b5d26
chiakiwang <110362018+chiakiwang@users.noreply.github.com>
2024-05-04 06:52:23 +0800

    Update 吸瓜.js

--- JS/吸瓜.js
+++ JS/吸瓜.js
@@ -7,7 +7,7 @@ var rule = {
     },
     timeout:5000,
     class_name:'女主播&情侣&男主播&变性人',//静态分类名称拼接
-    class_url:'最新av線上看&熱門av線上看&每日av線上看線上看&精選av線上看',//静态分类标识拼接
+    class_url:'中文&顶级免费现场视频&情侣性爱直播&最新 & 热门女主播&VR直播',//静态分类标识拼接
     limit:5,
     play_parse:true,
     lazy:'',
