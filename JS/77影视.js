c383f7d89a8f5ebb8f13869cd9565b8eee98f842
chiakiwang <110362018+chiakiwang@users.noreply.github.com>
2024-05-05 01:48:20 +0800

    Update 77影视.js

--- JS/77影视.js
+++ JS/77影视.js
@@ -4,7 +4,7 @@ var rule = {
 	title:'77电影网',
 	模板:'mxone5',
 	host:'https://www.77ys.me',
-	// url:'/vodshow/fyfilter-------.html',
+	// url:'/vodshow/filter-------.html',
 	searchUrl: '/vodsearch/--.html?wd=**',
 	filterable:1,//是否启用分类筛选,
 	filter_url:'{{fl.cateId}}-{{fl.area}}-{{fl.by or "time"}}-{{fl.class}}-{{fl.lang}}-{{fl.letter}}---fypage---{{fl.year}}',
