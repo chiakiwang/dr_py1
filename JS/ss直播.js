d1f4fd79e142006894619d50d19a832a884566d2
chiakiwang <110362018+chiakiwang@users.noreply.github.com>
2024-05-27 02:35:22 +0800

    Update ss直播.js

--- JS/ss直播.js
+++ JS/ss直播.js
@@ -1,7 +1,7 @@
 var rule = {
     title:'ss直播',
-    host:'http://api.hclyz.com:81',
-    homeUrl:'/mf/json.txt',
+    host:'http://api.hclyz.com:81/mf/json.txt',
+    homeUrl:'',
     url:'/mf/fyclass',
     class_name:'all',
     class_url:'json.txt',
